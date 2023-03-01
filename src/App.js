534714332ebd958c6a367d3e53d4fb7661b49206
Akhil Rawat <41101825+akhilrawat001@users.noreply.github.com>
2023-03-01 11:38:18 +0530

    Update routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './App.module.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 
 import DataGridWrapper from './components/DataGridWrapper/DataGridWrapper';
 
@@ -9,25 +9,23 @@ const apiUrl = dev ? 'http://0.0.0.0:3001/data' : 'https://datagrid-backend.glit
 
 function App() {
     return (
-        <Router>
-            <div className={styles.headerContainer}>
-                <header className={styles.header}>
-                    <h1>DataGrid</h1>
-                    <nav className={styles.navItems}>
-                        <p className={styles.navItem}>
-                            <a href="/datagrid">Large Table</a>
-                        </p>
-                        <p className={styles.navItem}>
-                            <a href="/datagrid/small">Small Table</a>
-                        </p>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path="/datagrid" element={<LargeTable/>}/>
-                    <Route path="/datagrid/small" element={<SmallTable/>}/>
-                </Routes>
-            </div>
-        </Router>
+        <div className={styles.headerContainer}>
+            <header className={styles.header}>
+                <h1>DataGrid</h1>
+                <nav className={styles.navItems}>
+                    <p className={styles.navItem}>
+                        <a href="/datagrid">Large Table</a>
+                    </p>
+                    <p className={styles.navItem}>
+                        <a href="/datagrid/small">Small Table</a>
+                    </p>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/datagrid" element={<LargeTable/>}/>
+                <Route path="/datagrid/small" element={<SmallTable/>}/>
+            </Routes>
+        </div>
     );
 }
 
